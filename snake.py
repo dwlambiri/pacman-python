2501687957921fac8fdab788dfd4100c2f7bf5d4
j792lee <59450686+j792lee@users.noreply.github.com>
2020-01-03 20:18:34 -0500

    Add files via upload

--- snake.py
+++ snake.py
@@ -101,15 +101,44 @@ def game(player1, screen):
         pygame.display.update()
         time.sleep(0.05)
 
+def nameMaker(screen, font):
+    nameAsciiValue = [65,65,65]#sets defaulf ascii to 'A'
+    nameAsciiIndex = 0
+    screen.blit(font.render("Name:", 200, (255, 255, 255)), (600, 150))
+    while True:
+        pygame.display.update()
+        rectangl = (700+nameAsciiIndex*20, 150, 25, 25)
+        pygame.draw.rect(screen, (0,0,0), rectangl)
+        screen.blit(font.render("{0}".format(chr(nameAsciiValue[nameAsciiIndex])), 200, (255, 255, 255)), (700+nameAsciiIndex*20, 150))
+        print(nameAsciiValue)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    nameAsciiValue[nameAsciiIndex]+= 1
+                    if nameAsciiValue[nameAsciiIndex] > 90: nameAsciiValue[nameAsciiIndex] = 65
+                elif event.key == pygame.K_DOWN:
+                    nameAsciiValue[nameAsciiIndex] -= 1
+                    if nameAsciiValue[nameAsciiIndex] < 65: nameAsciiValue[nameAsciiIndex] = 90
+                elif event.key == pygame.K_RETURN and nameAsciiIndex<2:
+                    nameAsciiIndex +=1
+                elif event.key == pygame.K_RETURN and nameAsciiIndex == 2:
+                    playerName = ''.join(chr(x) for x in nameAsciiValue)
+                    print(playerName)
+                    quit() #REMEMBER TO REMOVE QUIT
+                    return playerName
+            elif event.type == pygame.QUIT:
+                quit()
+
 pygame.init()
 pygame.font.init()
 font = pygame.font.SysFont("sans",40)
 player1 = Snake()
 screen = pygame.display.set_mode((30 * len(player1.bored[0]), (30 * len(player1.bored))))
-insertScore(game(player1, screen), "Justin")
+#insertScore(game(player1, screen), "Justin")
 
-player1.bored = [[0 for i in range(40)] for i in range(20)]
-drawbored(screen, player1.bored)
+#player1.bored = [[0 for i in range(40)] for i in range(20)]
+#drawbored(screen, player1.bored)
 
-displayHS(screen, font)
+nameMaker(screen, pygame.font.SysFont("sans",40))
+#displayHS(screen, font)
 
