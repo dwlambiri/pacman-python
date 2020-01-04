042aa1f5f98459341eead9d4e2751465f260725a
j792lee <59450686+j792lee@users.noreply.github.com>
2020-01-04 00:47:46 -0500

    Add files via upload

--- snake.py
+++ snake.py
@@ -110,7 +110,7 @@ def nameMaker(screen, font):
         rectangl = (200+nameAsciiIndex*300, 150, 400, 400)
         pygame.draw.rect(screen, (0,0,0), rectangl)
         screen.blit(font.render("{0}".format(chr(nameAsciiValue[nameAsciiIndex])), 200, (255, 255, 255)), (200+nameAsciiIndex*300, 150))
-        print(nameAsciiValue)
+
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
@@ -123,8 +123,6 @@ def nameMaker(screen, font):
                     nameAsciiIndex +=1
                 elif event.key == pygame.K_RETURN and nameAsciiIndex == 2:
                     playerName = ''.join(chr(x) for x in nameAsciiValue)
-                    print(playerName)
-                    #quit() #REMEMBER TO REMOVE QUIT
                     return playerName
             elif event.type == pygame.QUIT:
                 quit()
